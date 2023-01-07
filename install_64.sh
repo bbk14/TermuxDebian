e4f5ff5bdc9a5f2c8205ec832800b05ccf445bab
bbk14 <122102237+bbk14@users.noreply.github.com>
2023-01-07 20:10:24 +0200

    Update install_64.sh

--- install_64.sh
+++ install_64.sh
@@ -9,7 +9,7 @@ pkg install -y tmux proot-distro
 echo "tmux new-session -s Lampac -d "proot-distro login debian -- bash /root/cron.sh"" >> ~/.bashrc
 echo "tmux new-session -s Jackett -d "proot-distro login debian -- /home/Jackett/./jackett"" >> ~/.bashrc
 echo "tmux new-session -s Info -d "proot-distro login debian -- bash /root/information.sh"" >> ~/.bashrc
-echo "tmux attach -t Info" >> ~/.bashrc
+echo "tmux detach" >> ~/.bashrc
 source ~/.bashrc
 
 proot-distro install debian
