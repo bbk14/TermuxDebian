ea73f55b80072783e54b06ccd02fe0bac0688eaf
bbk14 <122102237+bbk14@users.noreply.github.com>
2023-01-07 20:33:12 +0200

    Update install_64.sh

--- install_64.sh
+++ install_64.sh
@@ -2,15 +2,69 @@
 BLUE='\033[1;36m'
 YELLOW='\033[1;33m'
 LAMPAC="/home/lampac"
+echo '#!/bin/bash
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+BLUE='\033[1;36m'
+YELLOW='\033[1;33m'
+echo ""
 
-pkg install -y wget tmux proot-distro
+echo -e "${RED}*****### Додатково ###*****"
+echo -e "${GREEN}Lampac та Jackett працюють у фоні"
 
-wget https://raw.githubusercontent.com/bbk14/Termux-Debian-Lampac-Jackett/main/information.sh -O information.sh
-chmod 755 information.sh
+echo ""
+
+echo -e "${BLUE}відключитись від підключеної сесії:"
+echo -e " ${YELLOW}Ctrl+B+D"
+
+echo -e "${BLUE}підключитись до сесії Lampac:"
+echo -e " ${YELLOW}tmux attach -t Lampac"
+
+echo ""
+
+echo -e "${BLUE}підключитись до сесії Jackett:"
+echo -e " ${YELLOW}tmux attach -t Jackett"
+
+echo ""
+
+echo -e "${BLUE}підключитись до сесії info:"
+echo -e " ${YELLOW}tmux attach -t Info"
+
+echo ""
+
+echo -e "${BLUE}зупинити всі сесії та програми:"
+echo -e " ${YELLOW}tmux kill-server"
+
+echo ""
+
+echo -e "${BLUE}запустити Debian для додаткових налаштувань:"
+echo -e " ${YELLOW}proot-distro login debian"
+
+echo ""
+
+echo -e "${BLUE}замінити APIKey Jackett:"
+echo -e " ${YELLOW}nano /root/.config/Jackett/ServerConfig.json"
+ 
+echo ""
+
+echo -e "${BLUE}редагувати конфігурацію Lampac:"
+echo -e " ${YELLOW}nano /home/lampac/init.conf"
+
+echo ""
+
+echo -e "${RED}про конфігурацію на сайті розробника:"
+echo -e " ${GREEN}https://github.com/immisterio/Lampac"
+
+echo ""
+echo -e "${BLUE}СЛАВА ${YELLOW}УКРАЇНІ"' > info.sh
+
+pkg install -y tmux proot-distro
+
+
 
 echo "tmux new-session -s Lampac -d "proot-distro login debian -- bash /root/cron.sh"" >> ~/.bashrc
 echo "tmux new-session -s Jackett -d "proot-distro login debian -- /home/Jackett/./jackett"" >> ~/.bashrc
-echo "bash information.sh"" >> ~/.bashrc
+echo "bash info.sh"" >> ~/.bashrc
 echo "tmux detach" >> ~/.bashrc
 source ~/.bashrc
 
