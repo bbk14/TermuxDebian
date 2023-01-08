25dc335923698d2029fece55eb68067b9fa8e331
bbk14 <122102237+bbk14@users.noreply.github.com>
2023-01-08 17:36:03 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 BLUE='\033[1;36m'
 YELLOW='\033[1;33m'
 VER="$(uname -m | grep aarch64)"
-pkg install -y tmux proot-distro
 if [[ "$VER" == "aarch64" ]];
 then
 curl -s https://raw.githubusercontent.com/bbk14/Termux-Debian-Lampac-Jackett/main/ver64.sh | bash
