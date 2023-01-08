3f4a3ccd6da8606786c3c641630da2e23320dadb
bbk14 <122102237+bbk14@users.noreply.github.com>
2023-01-08 12:09:05 +0200

    Update install_64.sh

--- install_64.sh
+++ install_64.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
-
 BLUE='\033[1;36m'
 YELLOW='\033[1;33m'
-
+#
 cd $HOME
 curl -s https://raw.githubusercontent.com/bbk14/Termux-Debian-Lampac-Jackett/main/info.sh > info.sh
 chmod 755 info.sh
 bash info.sh
-
+#
 pkg install -y tmux proot-distro
 echo "bash info.sh" >> ~/.bashrc
 echo "tmux new-session -s Lampac -d "proot-distro login debian -- bash /root/cron.sh"" >> ~/.bashrc
 echo "tmux new-session -s Jackett -d "proot-distro login debian -- /home/Jackett/./jackett"" >> ~/.bashrc
 echo "tmux detach" >> ~/.bashrc
 source ~/.bashrc
-
+#
 proot-distro install debian
 proot-distro login debian
-
+#
 apt-get update && apt-get install -y wget libicu67
 wget https://dot.net/v1/dotnet-install.sh
 chmod 755 dotnet-install.sh
 bash dotnet-install.sh --runtime aspnetcore --channel 6.0 --install-dir /usr/share/dotnet
 ln -s /usr/share/dotnet/dotnet /usr/bin/dotnet
-
+#
 wget https://raw.githubusercontent.com/bbk14/Termux-Debian-Lampac-Jackett/main/cron.sh
 chmod 755 cron.sh
-
+#
 cd /home
 mkdir lampac
 cd /home/lampac
@@ -37,7 +36,7 @@ wget https://raw.githubusercontent.com/bbk14/Termux-Debian-Lampac-Jackett/main/u
 chmod 755 update.sh
 cp example.conf init.conf
 echo '33' > vers.txt
-
+#
 cd /home
 wget https://github.com/Jackett/Jackett/releases/latest/download/Jackett.Binaries.LinuxARM64.tar.gz
 tar -xvf Jackett.Binaries.LinuxARM64.tar.gz
