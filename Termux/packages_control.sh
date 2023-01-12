2c2734a1cf3b41290ab3a0e9f0cd71789296241d
bbk14 <122102237+bbk14@users.noreply.github.com>
2023-01-12 13:36:36 +0200

    Update packages_control.sh

--- Termux/packages_control.sh
+++ Termux/packages_control.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 #color
-Install=$'\e[1;32mInstall\e[0;33m'
-Uninstall=$'\e[1;31mUninstall\e[0;33m'
-Quit=$'\e[1;41mQuit\e[0;33m'
 RED='\033[1;31m'
 GREEN='\033[1;32m'
 PURPLE='\033[0;35m'
@@ -11,7 +8,7 @@ NC='\033[0m'
 
 PS3=$'\e[1;33mWhat we do today: \e[0;33m'
 
-select option in "$Install" "$Uninstall" "$Quit"; do
+select option in Install Uninstall Quit; do
 case $option in
 Install)
 proot-distro login debian -- curl -s -J -O https://raw.githubusercontent.com/bbk14/TermuxDebian/main/Termux/Debian/install.sh
