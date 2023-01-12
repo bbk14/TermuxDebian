c14616db168728d7e8960b5c8c8b17d60505ec66
bbk14 <122102237+bbk14@users.noreply.github.com>
2023-01-12 23:17:11 +0200

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -5,10 +5,8 @@ proot-distro install debian
 #download scripts in Termux
 curl -s -O https://raw.githubusercontent.com/bbk14/TermuxDebian/main/Termux/note.sh
 chmod 755 note.sh
-curl -s -O https://raw.githubusercontent.com/bbk14/TermuxDebian/main/Termux/noteT.sh
-chmod 755 noteT.sh
-curl -s -O https://raw.githubusercontent.com/bbk14/TermuxDebian/main/Termux/noteD.sh
-chmod 755 noteD.sh
+curl -s -O https://raw.githubusercontent.com/bbk14/TermuxDebian/main/Termux/cronT.sh
+chmod 755 cronT.sh
 curl -s -O https://raw.githubusercontent.com/bbk14/TermuxDebian/main/Termux/tmux_off.sh
 chmod 755 tmux_off.sh
 curl -s -O https://raw.githubusercontent.com/bbk14/TermuxDebian/main/Termux/packages_control.sh
@@ -17,11 +15,16 @@ chmod 755 packages_control.sh
 proot-distro login debian
 #install packages in Debian
 apt-get update && apt-get install -y wget libicu67
+curl -s -O https://raw.githubusercontent.com/bbk14/TermuxDebian/main/Termux/cronD.sh
+chmod 755 cronD.sh
+cat <<EOT >> .bashrc
+cronD.sh
+EOT
 #exit from Debian
 exit
 #add scripts to autorun in Termux when it open
 cat <<EOT >> .bashrc
-curl -s -J -O https://raw.githubusercontent.com/bbk14/TermuxDebian/main/Termux/packages_control.sh
+cronT.sh
 bash tmux_off.sh
 #GreetingStart
 bash note.sh
