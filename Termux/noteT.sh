baa001ce165e234df71a39131bb4083a11e7f040
bbk14 <122102237+bbk14@users.noreply.github.com>
2023-01-12 14:20:17 +0200

    Update noteT.sh

--- Termux/noteT.sh
+++ Termux/noteT.sh
@@ -3,6 +3,7 @@ GREEN='\033[1;32m'
 BLUE='\033[1;36m'
 YELLOW='\033[1;33m'
 PURPLE='\033[0;35m'
+NC='\033[0m'
 echo -e "${GREEN}Termux input commands:"
 echo ""
 echo -e "${PURPLE}run packages control to install or unistall them:"
@@ -16,7 +17,7 @@ echo -e "${BLUE}disconnect from session when you in Tmux:"
 echo -e " ${YELLOW}Ctrl+B+D"
 echo ""
 echo -e "${BLUE}stop all sessions and packages:"
-echo -e " ${YELLOW}tmux kill-server"
+echo -e " ${YELLOW}tmux kill-server${NC}"
 #DebianStart
 echo ""
 echo -e "${BLUE}start Debian for more settings:"
