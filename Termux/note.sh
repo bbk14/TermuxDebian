1a35ea56b1519926afff4e3e936c3bfa8178d0c6
bbk14 <122102237+bbk14@users.noreply.github.com>
2023-01-13 19:12:45 +0200

    Update note.sh

--- Termux/note.sh
+++ Termux/note.sh
@@ -19,14 +19,11 @@ echo -e "${BLUE}connect to Lampac session:"
 echo -e " ${YELLOW}tmux attach -t Lampac${NC}"
 echo -e "${NC}${BLUE}change Lampac config:"
 echo -e " ${YELLOW}nano /home/lampac/init.conf${NC}"
-#Lampac autostart
-! grep -qF 'tmux new-session -s Lampac -d "proot-distro login debian -- bash /root/lampac_updater.sh"' '.bashrc' || echo "tmux new-session -s Lampac -d "proot-distro login debian -- bash /root/lampac_updater.sh"" >> .bashrc
-echo "tmux new-session -s Lampac -d "proot-distro login debian -- bash /root/lampac_updater.sh"" >> .bashrc
+sed -i '/-s Lampac/d' .bashrc
+echo 'tmux new-session -s Lampac -d "proot-distro login debian -- bash /root/lampac_updater.sh"' >> ~/.bashrc
 else
 sed -i '/-s Lampac/d' .bashrc
 fi
-#Lampac autostart
-
 #check if Jackett is installed
 if proot-distro login debian -- [ -d "/home/Jackett" ];
 then
@@ -37,12 +34,11 @@ echo -e "${BLUE}connect to Jackett session:"
 echo -e " ${YELLOW}tmux attach -t Jackett${NC}"
 echo -e "${NC}${BLUE}change APIKey Jackett:"
 echo -e " ${YELLOW}nano /root/.config/Jackett/ServerConfig.json${NC}"
-#Jackett autostart
-! grep -qF 'tmux new-session -s Jackett -d "proot-distro login debian -- /home/Jackett/./jackett"' '.bashrc' || echo "tmux new-session -s Jackett -d "proot-distro login debian -- /home/Jackett/./jackett"" >> .bashrc
+sed -i '/-s Jackett/d' .bashrc
+echo 'tmux new-session -s Jackett -d "proot-distro login debian -- /home/Jackett/./jackett"' >> ~/.bashrc
 else
 sed -i '/-s Jackett/d' .bashrc
 fi
-#Jackett autostart
 
 #check if Torrserver is installed
 if proot-distro login debian -- [ -d "/home/torrserver" ];
@@ -52,12 +48,11 @@ echo -e "${NC}${GREEN}Torrserver running in background ip:8091"
 echo -e "${GREEN}info Torrserver: ${RED}https://github.com/YouROK/TorrServer"
 echo -e "${BLUE}connect to Torrserver session:"
 echo -e " ${YELLOW}tmux attach -t Torrserver${NC}"
-#Torrserver autostart
-! grep -qF 'tmux new-session -s Torrserver -d "proot-distro login debian -- /home/torrserver/torrserver -p 8091"' '.bashrc' || echo "tmux new-session -s Torrserver -d "proot-distro login debian -- /home/torrserver/torrserver -p 8091"" >> .bashrc
+sed -i '/-s Torrserver/d' .bashrc
+echo 'tmux new-session -s Torrserver -d "proot-distro login debian -- /home/torrserver/torrserver -p 8091"' >> ~/.bashrc
 else
 sed -i '/-s Torrserver/d' .bashrc
 fi
-#Torrserver autostart
 
 #check if Midnight Commander is installed
 if proot-distro login debian -- [ -d "/etc/mc" ];
@@ -78,15 +73,16 @@ echo -e " ${YELLOW}tmux kill-server${NC}"
 echo ""
 echo -e "${NC}${BLUE}start Debian for more settings:"
 echo -e " ${YELLOW}proot-distro login debian${NC}"
+
 #autorun
 sed -i '/bash note.sh/d' .bashrc
 sed -i '/bash packages_control.sh/d' .bashrc
 echo "bash note.sh" >> .bashrc
 echo "bash packages_control.sh" >> .bashrc
 #autorun
+
 echo ""
-echo -e "${YELLOW}packages control to ${GREEN}install ${YELLOW}or ${RED}unistall"
+echo -e "${YELLOW}packages control to ${GREEN}install ${YELLOW}or ${RED}unistall${NC}"
 echo -e "${BLUE}*it run at every boot Termux"
 echo -e " ${YELLOW}bash packages_control.sh"
-echo -e "${YELLOW}packages control to ${GREEN}install ${YELLOW}or ${RED}unistall${NC}"
 echo ""
