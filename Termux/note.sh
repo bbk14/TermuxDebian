279f0f4f7009e7fce9fca70d360bb7587ea23b0c
bbk14 <122102237+bbk14@users.noreply.github.com>
2023-01-13 16:04:24 +0200

    Update note.sh

--- Termux/note.sh
+++ Termux/note.sh
@@ -13,12 +13,14 @@ if proot-distro login debian -- [ -d "/home/lampac" ];
 then
 echo ""
 echo -e "${NC}${GREEN}Lampac running in background ip:9118"
-echo -e "${RED}info Lampac: https://github.com/immisterio/Lampac"
+echo -e "${GREEN}info Lampac: ${RED}https://github.com/immisterio/Lampac"
 echo -e "${BLUE}connect to Lampac session:"
 echo -e " ${YELLOW}tmux attach -t Lampac${NC}"
 echo -e "${NC}${BLUE}change Lampac config:"
 echo -e " ${YELLOW}nano /home/lampac/init.conf${NC}"
-sed -i '$ s/$/tmux new-session -s Lampac -d "proot-distro login debian -- bash /root/lampac_updater.sh"/' .bashrc
+elif ! grep -qF 'tmux new-session -s Lampac -d "proot-distro login debian -- bash /root/lampac_updater.sh"' .bashrc
+then
+echo "tmux new-session -s Lampac -d "proot-distro login debian -- bash /root/lampac_updater.sh"" >> .bashrc
 else
 sed -i '/-t Lampac/d' .bashrc
 fi
@@ -27,12 +29,14 @@ if proot-distro login debian -- [ -d "/home/Jackett" ];
 then
 echo ""
 echo -e "${NC}${GREEN}Jackett running in background ip:9117"
-echo -e "${RED}info Jackett: https://github.com/Jackett/Jackett"
-echo -e "${BLUE}connect to Lampac session:"
+echo -e "${GREEN}info Jackett: ${RED}https://github.com/Jackett/Jackett"
+echo -e "${BLUE}connect to Jackett session:"
 echo -e " ${YELLOW}tmux attach -t Jackett${NC}"
 echo -e "${NC}${BLUE}change APIKey Jackett:"
 echo -e " ${YELLOW}nano /root/.config/Jackett/ServerConfig.json${NC}"
-sed -i '$ s/$/tmux new-session -s Jackett -d "proot-distro login debian -- /home/Jackett/./jackett"/' .bashrc
+elif ! grep -qF 'tmux new-session -s Jackett -d "proot-distro login debian -- /home/Jackett/./jackett"' .bashrc
+then
+echo "tmux new-session -s Jackett -d "proot-distro login debian -- /home/Jackett/./jackett"" >> .bashrc
 else
 sed -i '/-t Jackett/d' .bashrc
 fi
@@ -41,10 +45,12 @@ if proot-distro login debian -- [ -d "/home/torrserver" ];
 then
 echo ""
 echo -e "${NC}${GREEN}Torrserver running in background ip:8091"
-echo -e "${RED}info Torrserver: https://github.com/YouROK/TorrServer"
+echo -e "${GREEN}info Torrserver: ${RED}https://github.com/YouROK/TorrServer"
 echo -e "${BLUE}connect to Torrserver session:"
 echo -e " ${YELLOW}tmux attach -t Torrserver${NC}"
-sed -i '$ s/$/tmux new-session -s Torrserver -d "proot-distro login debian -- /home/torrserver/torrserver -p 8091"/' .bashrc
+elif ! grep -qF 'tmux new-session -s Torrserver -d "proot-distro login debian -- /home/torrserver/torrserver -p 8091"' .bashrc
+then
+echo "tmux new-session -s Torrserver -d "proot-distro login debian -- /home/torrserver/torrserver -p 8091"" >> .bashrc
 else
 sed -i '/-t Torrserver/d' .bashrc
 fi
@@ -56,7 +62,7 @@ echo -e "${NC}${BLUE}run Midnight Commander:"
 echo -e " ${YELLOW}proot-distro login debian -- mc${NC}"
 fi
 echo ""
-echo -e "${PURPLE}run packages control to install or unistall them:"
+echo -e "${YELLOW}run packages control to ${GREEN}install ${YELLOW}or ${RED}unistall ${YELLOW}them:"
 echo -e "${BLUE}*it run at every boot Termux"
 echo -e " ${YELLOW}bash packages_control.sh"
 echo ""
