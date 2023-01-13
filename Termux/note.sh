75f4e800cf4387acc16fdf104cb8bbe5729de9a5
bbk14 <122102237+bbk14@users.noreply.github.com>
2023-01-13 09:47:38 +0200

    Update note.sh

--- Termux/note.sh
+++ Termux/note.sh
@@ -18,7 +18,7 @@ echo -e "${BLUE}connect to Lampac session:"
 echo -e " ${YELLOW}tmux attach -t Lampac${NC}"
 echo -e "${NC}${BLUE}change Lampac config:"
 echo -e " ${YELLOW}nano /home/lampac/init.conf${NC}"
-grep -qxF 'include "tmux new-session -s Lampac -d "proot-distro login debian -- bash /root/lampac_updater.sh""' .bashrc || echo 'include "tmux new-session -s Lampac -d "proot-distro login debian -- bash /root/lampac_updater.sh""' >> .bashrc
+grep -qxF 'tmux new-session -s Lampac -d "proot-distro login debian -- bash /root/lampac_updater.sh"' .bashrc || echo 'tmux new-session -s Lampac -d "proot-distro login debian -- bash /root/lampac_updater.sh"' >> .bashrc
 else
 sed -i '/-t Lampac/d' .bashrc
 fi
@@ -32,7 +32,7 @@ echo -e "${BLUE}connect to Lampac session:"
 echo -e " ${YELLOW}tmux attach -t Jackett${NC}"
 echo -e "${NC}${BLUE}change APIKey Jackett:"
 echo -e " ${YELLOW}nano /root/.config/Jackett/ServerConfig.json${NC}"
-grep -qxF 'include "tmux new-session -s Jackett -d "proot-distro login debian -- /home/Jackett/./jackett""' .bashrc || echo 'include "tmux new-session -s Jackett -d "proot-distro login debian -- /home/Jackett/./jackett""' >> .bashrc
+grep -qxF 'tmux new-session -s Jackett -d "proot-distro login debian -- /home/Jackett/./jackett"' .bashrc || echo 'tmux new-session -s Jackett -d "proot-distro login debian -- /home/Jackett/./jackett"' >> .bashrc
 else
 sed -i '/-t Jackett/d' .bashrc
 fi
@@ -44,7 +44,7 @@ echo -e "${NC}${GREEN}Torrserver running in background ip:8091"
 echo -e "${RED}info Torrserver: https://github.com/YouROK/TorrServer"
 echo -e "${BLUE}connect to Torrserver session:"
 echo -e " ${YELLOW}tmux attach -t Torrserver${NC}"
-grep -qxF 'include "tmux new-session -s Torrserver -d "proot-distro login debian -- /home/torrserver/torrserver -p 8091""' .bashrc || echo 'include "tmux new-session -s Torrserver -d "proot-distro login debian -- /home/torrserver/torrserver -p 8091""' >> .bashrc
+grep -qxF 'tmux new-session -s Torrserver -d "proot-distro login debian -- /home/torrserver/torrserver -p 8091"' .bashrc || echo 'tmux new-session -s Torrserver -d "proot-distro login debian -- /home/torrserver/torrserver -p 8091"' >> .bashrc
 else
 sed -i '/-t Torrserver/d' .bashrc
 fi
