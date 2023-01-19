fd6154fd92b7097ed8e36590272486a3d9c3b3ae
bbk14 <122102237+bbk14@users.noreply.github.com>
2023-01-19 07:47:13 +0200

    Update updater.sh

--- Termux/updater.sh
+++ Termux/updater.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-note=$(curl --head --silent https://raw.githubusercontent.com/bbk14/TermuxDebian/main/Termux/note.sh | head -n 1)
-packages=$(curl --head --silent https://raw.githubusercontent.com/bbk14/TermuxDebian/main/Termux/packages.sh | head -n 1)
+notecheck=$(curl --head --silent https://raw.githubusercontent.com/bbk14/TermuxDebian/main/Termux/note.sh | head -n 1)
+packagescheck=$(curl --head --silent https://raw.githubusercontent.com/bbk14/TermuxDebian/main/Termux/packages.sh | head -n 1)
+note=$(if echo "$notecheck" | grep -q 200 ; then curl -s -J -O https://raw.githubusercontent.com/bbk14/TermuxDebian/main/Termux/note.sh ; fi)
+packages=$(if echo "$packagescheck" | grep -q 200 ; then curl -s -J -O https://raw.githubusercontent.com/bbk14/TermuxDebian/main/Termux/packages.sh ; fi)
+google=$(curl -I --head --silent https://raw.githubusercontent.com/bbk14/TermuxDebian/main/Termux/note.sh | head -n 1)
 
-while true; do
-
-if echo "$note" | grep -q 200
+if echo "$google" | grep -q 200
 then
-curl -s -J -O https://raw.githubusercontent.com/bbk14/TermuxDebian/main/Termux/note.sh
+echo "Update"
+echo ""
+$note
+$packages
+else
+echo "No internet connection"
+echo ""
 fi
-
-if echo "$packages" | grep -q 200
-then
-curl -s -J -O https://raw.githubusercontent.com/bbk14/TermuxDebian/main/Termux/packages.sh
-fi
-
-sleep 5d #It runs for 5 days before restarting to check for updates or at every boot Termux
-
-done
