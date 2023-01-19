e60ac0dafc09d7604ba942f06052c00aa5e1e9b4
bbk14 <122102237+bbk14@users.noreply.github.com>
2023-01-19 07:12:07 +0200

    Update updater.sh

--- Termux/updater.sh
+++ Termux/updater.sh
@@ -2,6 +2,8 @@
 note=$(curl --head --silent https://raw.githubusercontent.com/bbk14/TermuxDebian/main/Termux/note.sh | head -n 1)
 packages=$(curl --head --silent https://raw.githubusercontent.com/bbk14/TermuxDebian/main/Termux/packages.sh | head -n 1)
 
+while true; do
+
 if echo "$note" | grep -q 200
 then
 curl -s -J -O https://raw.githubusercontent.com/bbk14/TermuxDebian/main/Termux/note.sh
@@ -11,3 +13,7 @@ if echo "$packages" | grep -q 200
 then
 curl -s -J -O https://raw.githubusercontent.com/bbk14/TermuxDebian/main/Termux/packages.sh
 fi
+
+sleep 5d #It runs for 5 days before restarting to check for updates or at every boot Termux
+
+done
