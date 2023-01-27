179e8839a764bc08c6e199d891612b2dd952781d
bbk14 <122102237+bbk14@users.noreply.github.com>
2023-01-27 09:30:38 +0200

    Update mini.sh

--- Termux/mini.sh
+++ Termux/mini.sh
@@ -2,14 +2,16 @@
 RED='\033[1;31m'
 GREEN='\033[1;32m'
 #install packages in Termux
+chmod 777 -R $HOME/torrserver
+chmod 777 -R $HOME/config/torrserver
 mkdir $HOME/config/torrserver
 mkdir $HOME/torrserver
 cd $HOME/torrserver
 curl -s -O https://www.dropbox.com/s/r7u8f13didnkatz/118OE_TorrServer-android-arm7
 mv 118OE_TorrServer-android-arm7 torrserver
 echo -n 118OE > vers.txt
-chmod 755 -R $HOME/torrserver
-chmod 755 -R $HOME/config/torrserver
+chmod 777 -R $HOME/torrserver
+chmod 777 -R $HOME/config/torrserver
 
 cat <<\EOF>> .bashrc
 pkg clean
