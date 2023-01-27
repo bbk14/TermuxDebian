43de6f6f1b8ed1bd7122a3c93b7a129bff7dd7bd
bbk14 <122102237+bbk14@users.noreply.github.com>
2023-01-27 09:11:44 +0200

    Add files via upload

---
+++ Termux/mini.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+#install packages in Termux
+mkdir $HOME/config/torrserver
+mkdir $HOME/torrserver
+cd $HOME/torrserver
+curl -s -O https://www.dropbox.com/s/r7u8f13didnkatz/118OE_TorrServer-android-arm7
+mv 118OE_TorrServer-android-arm7 torrserver
+echo -n 118OE > vers.txt
+chmod 755 -R $HOME/torrserver
+chmod 755 -R $HOME/config/torrserver
+
+cat <<\EOF>> .bashrc
+pkg clean
+$HOME/torrserver/torrserver -p 8091 -a -d $HOME/config/torrserver
+EOF
+
+#clean packages cache Termux
+pkg clean
+#Done
+echo ""
+echo "#############################################"
+echo -e "${GREEN}***####Done####***"
+echo -e "${RED}close Termux and open it again to apply changes!"
+echo -e "${GREEN}type to close: ${RED}exit${NC}"
