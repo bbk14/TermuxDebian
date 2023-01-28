55cc92f0ad63632b63d85e10c14f547cc8b7f6e2
bbk14 <122102237+bbk14@users.noreply.github.com>
2023-01-28 16:28:46 +0200

    Update atv.sh

--- Termux/ATV/atv.sh
+++ Termux/ATV/atv.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 RED='\033[1;31m'
 GREEN='\033[1;32m'
-termux-setup-storage
-wait $!
 #install backup Termux
-curl -s https://www.dropbox.com/s/5ie7s3f4cm1g3ag/termux.tar.gz --output /sdcard/termux.tar.gz
+curl -s https://www.dropbox.com/s/5ie7s3f4cm1g3ag/termux.tar.gz --output termux.tar.gz
+chmod +rx termux.tar.gz
 tar -zxf /sdcard/termux.tar.gz -C /data/data/com.termux/files --recursive-unlink --preserve-permissions
+wait $!
 #install backup Debian
-curl -s https://www.dropbox.com/s/qezfhj2iu4ynnr2/debian.tar.gz --output /sdcard/debian.tar.xz
-proot-distro restore /sdcard/debian.tar.xz
+curl -s https://www.dropbox.com/s/qezfhj2iu4ynnr2/debian.tar.gz --output debian.tar.xz
+chmod +rx debian.tar.xz
+proot-distro restore debian.tar.xz
+wait $!
 #clean cache Termux
-rm /sdcard/termux.tar.gz
-rm /sdcard/debian.tar.xz
+rm termux.tar.gz
+rm debian.tar.xz
 pkg clean
 #Done
 echo ""
