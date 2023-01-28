07e0380f4fe99d33c69f60ba61112f56e502013b
bbk14 <122102237+bbk14@users.noreply.github.com>
2023-01-28 16:08:49 +0200

    Update atv.sh

--- Termux/ATV/atv.sh
+++ Termux/ATV/atv.sh
@@ -2,10 +2,10 @@
 RED='\033[1;31m'
 GREEN='\033[1;32m'
 #install backup Termux
-curl -O https://www.dropbox.com/s/5ie7s3f4cm1g3ag/termux.tar.gz
+curl -s -O https://www.dropbox.com/s/5ie7s3f4cm1g3ag/termux.tar.gz
 tar -zxf termux.tar.gz -C /data/data/com.termux/files --recursive-unlink --preserve-permissions
 #install backup Debian
-curl -O https://www.dropbox.com/s/qezfhj2iu4ynnr2/debian.tar.gz
+curl -s -O https://www.dropbox.com/s/qezfhj2iu4ynnr2/debian.tar.gz
 proot-distro restore debian.tar.xz
 #clean cache Termux
 rm termux.tar.gz
